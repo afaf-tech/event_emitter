645bfd542e7b5f117722ea9d495cd7fd5d044061
fatihalfikri <fatihalfikri5@gmail.com>
2021-01-06 08:13:16 +0700

    DI mysql

--- design patterns/dependency injection/di.js
+++ design patterns/dependency injection/di.js
@@ -10,9 +10,40 @@ function userModel (options){
 
     db = options.db;
 
+    db.connect(function(err) {
+        if (err) throw err;
+        console.log('connected as id ' + db.threadId);
+    });
+
     return {
-        create: function(done){
-            db.query('INSERT ...', done)
+        create: function(quer){
+            db.query('INSERT INTO users SET ?', {name: 'test', email: 'test@gmail.com'}, function (error, results, fields) {
+                if (error) throw error;
+                console.log(results.insertId);
+              });
+        },
+        select: function(){
+            db.query('SELECT * FROM users', function (error, results, fields) {
+                if (error) throw error;
+                console.log('The solution is: ', results);
+                return results;
+            });
+        },
+        
+        find: function(id){
+            var sql    = 'SELECT * FROM users WHERE id = ' + db.escape(id);
+            db.query(sql, function(err, results, fields){
+                if (err) throw err;
+                var resultArray = Object.values(JSON.parse(JSON.stringify(results)))
+                console.log(resultArray[0]);
+                return resultArray[0]
+            })
+        },
+        delete : function(id){
+            db.query(`DELETE FROM posts WHERE id = ${id}`, function (error, results, fields) {
+                if (error) throw error;
+                console.log('deleted ' + results.affectedRows + ' rows');
+            })
         }
     }
 }
--- design patterns/dependency injection/exec.js
+++ design patterns/dependency injection/exec.js
@@ -1,4 +1,15 @@
-var db = require('./db');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'aan_ppob'
+});
+
 var userModel = require('./di')({
-    db:db
+    db:connection
 })
+
+// userModel.select();
+var user1 =userModel;
+user1.select();
